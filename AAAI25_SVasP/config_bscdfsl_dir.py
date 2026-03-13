14f65fb2aa285b0cd3c7e72be5e3d91567eedfc1
wenqian li <wenqianli.seu@gmail.com>
2026-03-13 21:42:30 +0800

    Delete AAAI25_SVasP directory

--- AAAI25_SVasP/config_bscdfsl_dir.py
+++
@@ -1,6 +0,0 @@
-miniImageNet_path           	= '/data/user/dataset/miniImagenet'
-
-ISIC_path = "/data/user/dataset/ISIC"
-ChestX_path = "/data/user/dataset/ChestX"
-CropDisease_path = "/data/user/dataset/CropDisease"
-EuroSAT_path = "/data/user/dataset/EuroSAT/2750"
